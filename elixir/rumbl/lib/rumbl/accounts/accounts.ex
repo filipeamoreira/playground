00ec19bec19a2c02b9299e917cf72a7efd7db0e4
Filipe A Moreira <code@filipeamoreira.com>
2020-01-15 12:22:46 +0000

    Hook up Ecto

--- elixir/rumbl/lib/rumbl/accounts/accounts.ex
+++ elixir/rumbl/lib/rumbl/accounts/accounts.ex
@@ -3,23 +3,32 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Pepe Reina", username: "pepereina"},
-      %User{id: "2", name: "Will Smith", username: "willsmith"},
-      %User{id: "3", name: "Tina Turner", username: "tinaturner"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
